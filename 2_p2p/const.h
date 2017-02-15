d4ee3219772c1e07b63396044599433fd5da1f02
Matt Mikuta <mmikuta@hawk.iit.edu>
2017-02-14 22:00:38 -0600

    Added initial commmit for project 2, based off of 1

---
+++ 2_p2p/const.h
@@ -0,0 +1,17 @@
+//Maximum filename length
+#define MAXLINE 1024
+//Maximum hostname length
+#define HOSTNAMELENGTH 16
+//Number of clients
+#define NUMCLIENTS 3
+//Number of threads for client, minimum = 3
+#define NTHREADS NUMCLIENTS+2
+//File size significant digits; Ex 1024 = 4
+#define MAXFILESIZECHARS 256
+//Size of file transfer chunk
+#define BUFFSIZE 512
+//Number of bytes to hold number of clients to retrieve from
+//MAX = PEERRECVNUMCHARS*10^8. Now its 99999999
+#define PEERRECVNUMCHARS 2
+//Maximum files user can register
+#define MAXUSRFILES 10
