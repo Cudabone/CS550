4660cb13d40946db3eb21a88b44ee49b593f9e11
Matt Mikuta <mmikuta@hawk.iit.edu>
2017-03-11 14:17:59 -0600

    Moved run scripts into script directory, fixed setup.sh to compensate

--- 3_p2p/setup.sh
+++ 3_p2p/setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 make
+echo Moving Client into directories
 cp ./client ./c1/
 cp ./config/line1.txt ./c1/
-cp ./run_center.sh ./c1/
 cp ./client ./c2/
-cp ./run_leaf2.sh ./c2/
 cp ./config/line2.txt ./c2/
 cp ./client ./c3/
 cp ./config/line3.txt ./c3/
-cp ./run_leaf3.sh ./c3/
 cp ./client ./c4/
-cp ./run_leaf4.sh ./c4/
 cp ./client ./c5/
-cp ./run_leaf5.sh ./c5/
+
+echo Moving run scripts into directories
+cd scripts
+cp ./run_center.sh ../c1/
+cp ./run_leaf2.sh ../c2/
+cp ./run_leaf3.sh ../c3/
+cp ./run_leaf4.sh ../c4/
+cp ./run_leaf5.sh ../c5/
+cd ..
 
 echo Copying test files
 cd testfiles
