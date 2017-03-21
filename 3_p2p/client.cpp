e0c6b9fac40a9695ec37800714ce1be52373cdc7
Matt Mikuta <mmikuta@hawk.iit.edu>
2017-03-21 19:47:37 -0400

    fixed unneccessary comment

--- 3_p2p/client.cpp
+++ 3_p2p/client.cpp
@@ -555,7 +555,6 @@ void client_user()
 					//Call recv file here
 					if(input == 1)
 					{
-						//TODO may not need new 
 						in_port_t port = (in_port_t)atoi(peerid);
 						sa.sin_port = htons(port);
 						err = connect(sfd,(const struct sockaddr *)&sa,sizeof(sa));
